03176daeeed06e7681887bc8d53a4986c55d1365
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-09-09 22:57:30 +0100

    set pxl->unit ratio, add dirt with mass

---
+++ Assets/Scripts/Dirt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dirt : MonoBehaviour {
+
+    public float mass = 100f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    // Update is called once per physics engine execution
+    void FixedUpdate() {
+        if (mass <= 0f) {
+            Destroy(gameObject);
+        }
+    }
+
+    void AttackedWithForce(float force) {
+        mass -= force;
+
+    }
+}
